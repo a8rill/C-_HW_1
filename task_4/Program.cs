1a8659824873fa993d4bf330f984e128b8bf5cba
Olua <a8rill@mail.ru>
2023-02-13 21:11:54 +0300

    added task_4 project

---
+++ task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+Console.WriteLine("Введите целое число:");
+int number_A = Convert.ToInt32(Console.ReadLine());
+
+for (int i=1; i<=number_A; i++)
+if (i % 2==0)
+{
+ Console.WriteLine($"Число {i} - чётное");
+ }
+
